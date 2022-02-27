06dba5b36e1c5efd2caffe1cabceeec5431c6efb
DymaKot136 <97734033+DymaKot136@users.noreply.github.com>
2022-02-28 00:23:06 +0100

    Delete EduHacks/html directory

--- EduHacks/html/logout.php
+++
@@ -1,7 +0,0 @@
-<?php
-    $nomUsuari = $_POST["nomUsuari"];
-    @session_start();
-    session_destroy();
-    setcookie($nomUsuari, "cookie", time() - (3600*24));
-    header("Location:./іndex.php");
-?>
